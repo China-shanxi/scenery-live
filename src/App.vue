d30eaae14b667f8d35d536f931ecae048a3025f9
China-shanxi <3424297083@qq.com>
2026-07-17 11:48:22 +0800

    Add App.vue with template and Navbar component

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+    <main class="flex-1 container mx-auto px-4 py-6">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
